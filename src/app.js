ab5f74be3d23bd48c802b4d7510af9489f27d7b3
artG-acc <artemio.guerrero@g.austincc.edu>
2026-02-23 16:01:53 -0600

    Add EJS layout, partials, and Bootstrap homepage

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
+const ejsMate = require('ejs-mate');
 
 connectDB();
 
 // view engine
+app.engine('ejs',ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));
 
